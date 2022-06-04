bd2315e1e921d5dc8433931655051df6f0143fa3
Nicholas <nicholas0295@gmail.com>
2022-06-04 10:59:17 +0800

    modified comments

--- src/funcs/data.cpp
+++ src/funcs/data.cpp
@@ -4,7 +4,7 @@ float vertices[] = {
   -0.5f, -0.5f,  0.0f,
   0.0f, 0.0f, 0.0f
 };
-unsigned int indices[] = {  // note that we start from 0!
+unsigned int indices[] = {
     0, 1, 2,   // first triangle
     1, 2, 3
 };
