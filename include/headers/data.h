5eca6b6649080f90961f1cabf49c5f4a5f80914f
Nicholas <nicholas0295@gmail.com>
2022-06-07 18:37:27 +0800

    fixed texCoord bound error

--- include/headers/data.h
+++ include/headers/data.h
@@ -1,5 +1,7 @@
 #pragma once
 //data
-extern float vertices[12];
+extern float vertices[32];
 extern unsigned int indices[6];
 extern float colours[12];
+extern float texCoords[8];
+extern float blend;
